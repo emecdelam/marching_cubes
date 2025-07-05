556b0f3b4e7043ec95301ec3466a4e85f9f2227f
emecdelam <kiwi.thefruit0000@gmail.com>
2025-07-05 19:37:00 +0200

    changing delay at start

--- src/constants.zig
+++ src/constants.zig
@@ -3,7 +3,8 @@ pub const WINDOW_HEIGHT = 1080;
 
 pub const MOVEMENT_SPEED = 0.1;
 
-pub const NUMBER_NODE = 4;
+pub const NUMBER_NODE = 6;
 pub const NODE_SPACING = 1.0;
 
 pub const STEP_TIME = 0.20;
+pub const TIME_DELAY = 1.0;
